a16e99617ce0b6a631e6a1526f2ef322c725d2b5
EWB <ewil@stlawu.edu>
2015-05-08 12:32:33 -0400

    updates to page template and addition of parallax background module

--- sites/all/themes/digital/templates/page/page.tpl.php
+++ sites/all/themes/digital/templates/page/page.tpl.php
@@ -10,6 +10,15 @@
  * @see html.tpl.php
  */
 ?>
+<!-- ewb -->
+<div id="top_bar_container">
+      <div id="top_bar">
+        <div id="slu">
+          <a href="http://www.stlawu.edu"><img id="stlaw" src="http://dev.stlawu.edu/ksp/sites/all/themes/digitalradix/images/slu-horizontal-white.png" alt="St. Lawrence University" height="38"/></a>
+        </div>
+      </div> 
+</div>       
+<!-- ewb done -->
 <header id="header" class="header" role="header">
   <div class="container">
     <nav class="navbar navbar-default" role="navigation">
