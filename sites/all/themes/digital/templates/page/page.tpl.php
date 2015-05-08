c05f33cd32faf216e2b5c18f92b49a9e40693d86
EWB <ewil@stlawu.edu>
2015-05-08 13:22:40 -0400

    page template tweaks to add relative path to slu image

--- sites/all/themes/digital/templates/page/page.tpl.php
+++ sites/all/themes/digital/templates/page/page.tpl.php
@@ -14,7 +14,7 @@
 <div id="top_bar_container">
       <div id="top_bar">
         <div id="slu">
-          <a href="http://www.stlawu.edu"><img id="stlaw" src="http://dev.stlawu.edu/ksp/sites/all/themes/digitalradix/images/slu-horizontal-white.png" alt="St. Lawrence University" height="38"/></a>
+          <a href="http://www.stlawu.edu"><img id="stlaw" src="<?php print base_path(); ?>sites/all/themes/digital/images/slu-horizontal-white.png" alt="St. Lawrence University" height="38"/></a>
         </div>
       </div> 
 </div>       
